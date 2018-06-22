8d4fb0daee9a93eba28e7def9cabdf6081865189
sbti <dti@sitiobarreiras.com.br>
2018-06-22 14:03:37 -0300

    SectionAdapter added

---
+++ app/src/main/java/br/com/sbs/android_lib_util/adapter/SectionFragmentAdapter.java
@@ -0,0 +1,36 @@
+package br.com.sbs.android_lib_util.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+public class SectionFragmentAdapter extends FragmentStatePagerAdapter{
+
+    List<Fragment> fragments;
+    List<String> titles;
+
+    public SectionFragmentAdapter(FragmentManager fm, List<String> titles, List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+        this.titles = titles;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles.get(position);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments == null ? 0: fragments.size();
+    }
+
+
+}
